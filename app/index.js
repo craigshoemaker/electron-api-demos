35214c5e670c7b2ee803cb14633558f00969bc61
Craig Shoemaker <craig@craigshoemaker.net>
2015-07-22 15:14:08 -0700

    updated index.html

--- app/index.js
+++ app/index.js
@@ -1,9 +1,1 @@
-document.write('The current version is ' + process.version);
-
-var fs = require('fs');
-var path = require('path');
-
-var fullPath = path.resolve(__dirname, '../package.json');
-
-var pkg = fs.readFileSync(fullPath, 'utf8');
-alert(pkg);
+/// <reference path="../typings/node/node.d.ts"/>
