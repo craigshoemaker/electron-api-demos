ff2cc61f6ddb54ada0092daf428b72905e169b06
Craig Shoemaker <craig@craigshoemaker.net>
2015-07-23 06:54:34 -0700

    implemented dialogs.showOpenFile

--- app/index.js
+++ app/index.js
@@ -1,1 +1,14 @@
 /// <reference path="../typings/node/node.d.ts"/>
+(function(module){
+	
+	'use strict';
+	var path = require('path');
+	var dialogs = require(path.resolve(__dirname, './modules/dialogs'));
+	
+	dialogs.showOpenFile().then(function(){
+		
+		console.log('opened');
+		
+	});
+	
+}(module.exports));
---
+++ app/modules/dialogs/index.js
@@ -0,0 +1,29 @@
+(function(module){
+	
+	'use strict';
+	
+	var Q = require('q');
+	var _ = require('lodash');
+	var remote = require('remote');
+	var dialog = remote.require('dialog');
+	
+	module.showOpenFile = function(){
+		var deferred = Q.defer();
+		dialog.showOpenDialog({
+			properties: [ 'openFile' ], 
+			filters: [
+   				{ name: 'text', extensions: ['txt'] }
+		   ]
+	   },function(filePaths){
+		   
+			if(_.isUndefined(filePaths)) {
+				deferred.reject()
+			};
+			
+			deferred.resolve();
+		});
+		
+		return deferred.promise;
+	};
+	
+}(module.exports));
