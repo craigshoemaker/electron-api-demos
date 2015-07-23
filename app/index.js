686a8a4e44ed11496b3816bc17e6e2a86bb8a776
Craig Shoemaker <craig@craigshoemaker.net>
2015-07-23 11:34:08 -0700

    implemented all dialogs

--- app/index.js
+++ app/index.js
@@ -1,14 +1,87 @@
+/* global $ */
 /// <reference path="../typings/node/node.d.ts"/>
-(function(module){
-	
+(function (window, document, module) {
+
 	'use strict';
-	var path = require('path');
-	var dialogs = require(path.resolve(__dirname, './modules/dialogs'));
-	
-	dialogs.showOpenFile().then(function(){
+
+	var paths = {
+		jquery: './bower_components/jquery/dist/jquery.js',
+		dialogs: './modules/dialogs'
+	};
+
+	window.$ = window.jQuery = require.main.require(paths.jquery);
+
+	var dialogs = require.main.require(paths.dialogs);
+
+	$(function () {
+
+		var $result = $('#result');
+		var $openFileButton = $('#open-file-button');
+		var $openDirectoryButton = $('#open-directory-button');
+		var $createDirectoryButton = $('#create-directory-button');
+		var $saveFileButton = $('#save-file-button');
+		var $errorButton = $('#error-button');
+		var $messageBox = $('#message-box-button');
+
+		$openFileButton.click(function () {
+			
+			var options = {
+				title: 'Open Markdown Files',
+				filters: [{ name: 'Markdown', extensions: ['md'] }] 
+			};
+
+			dialogs.openFile(options).then(function (contents) {
+				$result.text(contents);
+			});
+		});
+
+		$openDirectoryButton.click(function () {
+			$result.html('');
+			dialogs.openDirectory().then(function (directoryPaths) {
+				directoryPaths.forEach(function (directoryPath, index) {
+					$result.append('<li>' + directoryPath);
+				});
+			});
+		});
 		
-		console.log('opened');
+		$createDirectoryButton.click(function () {
+			dialogs.createDirectory().then(function (directoryPath) {
+				$result.append('<li>' + directoryPath);
+			});
+		});
 		
+		$saveFileButton.click(function () {
+			var options = {
+				title: 'Save Markdown File',
+				filters: [{ name: 'Markdown', extensions: ['md', 'markdown']}]
+			};
+			
+			var content = 'This is a test';
+			
+			dialogs.saveFile(content, options, 'md').then(function (directoryPath) {
+				$result.text('File saved');
+			});
+		});
+		
+		$errorButton.click(function(){
+			dialogs.error('Big error', 'This is a test of the emergency broadcast system.\n\nThis is a second line.');
+		});
+		
+		$messageBox.click(function () {
+			
+			var options = {
+				'type': 'none',
+				buttons: ['Button: Index 0', 'Button: Index 1'], 
+				title: 'Custom Message Box',
+				message: 'Which button do you want to select?',
+				detail: 'Choose wisely!',
+				filters: [{ name: 'Markdown', extensions: ['md', 'markdown']}]
+			};
+			
+			dialogs.messageBox(options).then(function (buttonIndex) {
+				$result.text('Button index: ' + buttonIndex);
+			});
+		});
 	});
-	
-}(module.exports));
+
+} (window, document, module.exports));
--- app/modules/dialogs/index.js
+++ app/modules/dialogs/index.js
@@ -1,31 +1,130 @@
-(function(module){
-	
+(function (module) {
+
 	'use strict';
-	
+
 	var Q = require('q');
 	var _ = require('lodash');
+	var fs = require('fs');
+
 	var remote = require('remote');
 	var dialog = remote.require('dialog');
-	
-	module.showOpenFile = function(){
+
+	module.openFile = function (options) {
 		var deferred = Q.defer();
 
+		var dialogOptions = {
+			properties: ['openFile']
+		};
 		
-		dialog.showOpenDialog({
-			properties: [ 'openFile' ], 
-			filters: [
-   				{ name: 'text', extensions: ['txt'] }
-		   ]
-	   },
-	   
-	   
-	   function(filePaths){
-			if(_.isUndefined(filePaths)) {
+		if(!_.isUndefined(options)){
+			dialogOptions = _.defaults(dialogOptions, options);
+		}
+
+		var callback = function (filePaths) {
+			if (_.isUndefined(filePaths)) {
 				deferred.reject()
-			};
-			deferred.resolve();
-		});
+			} else {
+				var filePath = filePaths[0];
+				fs.readFile(filePath, 'utf8', function (err, contents) {
+					if (err) deferred.reject(err);
+					deferred.resolve(contents);
+				});
+			}
+		};
+
+		dialog.showOpenDialog(dialogOptions, callback);
+
+		return deferred.promise;
+	};
+
+	module.openDirectory = function (options) {
+		var deferred = Q.defer();
+
+		var dialogOptions = {
+			properties: ['openDirectory', 'multiSelections']
+		};
+		
+		if(!_.isUndefined(options)){
+			dialogOptions = _.defaults(dialogOptions, options);
+		}
+
+		var callback = function (directoryPaths) {
+			directoryPaths = (_.isUndefined(directoryPaths)) ? [] : directoryPaths;
+			deferred.resolve(directoryPaths);
+		};
+
+		dialog.showOpenDialog(dialogOptions, callback);
+
 		return deferred.promise;
 	};
 	
-}(module.exports));
+	module.createDirectory = function(options){
+		var deferred = Q.defer();
+
+		var dialogOptions = {
+			properties: ['createDirectory']
+		};
+		
+		if(!_.isUndefined(options)){
+			dialogOptions = _.defaults(dialogOptions, options);
+		}
+
+		var callback = function (directoryPath) {
+			deferred.resolve(directoryPath);
+		};
+
+		dialog.showOpenDialog(dialogOptions, callback);
+
+		return deferred.promise;
+	};
+	
+	module.saveFile = function(content, options, defaultExtension){
+		var deferred = Q.defer();
+
+		var dialogOptions = {};
+		
+		if(!_.isUndefined(options)){
+			dialogOptions = _.defaults(dialogOptions, options);
+		}
+
+		var callback = function (filePath) {
+			if(!_.isUndefined(defaultExtension)){
+				filePath = (_.endsWith(filePath, defaultExtension))? filePath : filePath + '.' + defaultExtension;
+			}
+			fs.writeFile(filePath, content, 'utf8', function(err){
+				if(err){
+					deferred.reject(err);
+				} else {
+					deferred.resolve(filePath);
+				}			
+			});
+		};
+
+		dialog.showSaveDialog(dialogOptions, callback);
+
+		return deferred.promise;
+	};
+	
+	module.messageBox = function(options){
+		var deferred = Q.defer();
+
+		var dialogOptions = {};
+		
+		if(!_.isUndefined(options)){
+			dialogOptions = _.defaults(dialogOptions, options);
+		}
+
+		var callback = function (buttonIndex) {
+			deferred.resolve(buttonIndex);
+		};
+
+		dialog.showMessageBox(dialogOptions, callback);
+
+		return deferred.promise;
+	};
+	
+	module.error = function(title, content){
+		dialog.showErrorBox(title, content);
+	};
+
+} (module.exports));
